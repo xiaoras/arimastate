e20a3256ae5724b16349ad5f8365cece95d3f840
Andrea Dapor <andrea.dapor@inverence.com>
2020-11-20 11:46:56 +0100

    basic files added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='arimaspace',
+      version='0.1',
+      description='python module for state space formulation of arima models',
+      url='http://github.com/xiaoras/arimaspace',
+      author='Andrea Dapor',
+      author_email='andrea.dapor@gmail.com',
+      license='',
+      packages=['arimaspace'],
+      zip_safe=False
+)
