b81b94d8c3d4002cbd01ea6be678004d17742f6b
Andrea Dapor <andrea.dapor@inverence.com>
2020-11-20 12:17:26 +0100

    other minor changes in metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name='arimastate',
-      version='0.1',
-      description='python module for state space formulation of arima models',
-      url='http://github.com/xiaoras/arimaspace',
-      author='Andrea Dapor',
-      author_email='andrea.dapor@gmail.com',
-      license='',
-      packages=['arimastate'],
-      zip_safe=False
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arimastate",
+    version="0.0.1",
+    author="Andrea Dapor",
+    author_email="andrea.dapor@gmail.com",
+    description="python module for state space formulation of arima models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xiaoras/arimastate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
